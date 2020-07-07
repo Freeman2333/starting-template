7c719c0736129d8322d055d3dd6528c8b53c2c74
freeman <freeman23@bigmir.net>
2020-07-07 15:40:00 +0300

    pug added & media querry mixin added

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,15 @@ const imagemin     = require('gulp-imagemin');
 const newer        = require('gulp-newer');
 const rsync        = require('gulp-rsync');
 const del          = require('del');
+const htmlValidator = require('gulp-w3c-html-validator');
+const plumber = require('gulp-plumber');
+const pug = require('gulp-pug');
+const argv = require('yargs').argv;
+const gulpif = require('gulp-if');
+const svgSprite = require('gulp-svg-sprite');
+const svgmin = require('gulp-svgmin');
+const cheerio = require('gulp-cheerio');
+const replace = require('gulp-replace');
 
 // Local Server
 
@@ -38,6 +47,17 @@ function styles() {
 	.pipe(dest('app/css'))
 	.pipe(browserSync.stream())
 }
+
+// Pug
+
+function pug2html() {
+  return src('app/pug/*.pug')
+    .pipe(plumber())
+    .pipe(pug({pretty: true}))
+    .pipe(plumber.stop())
+    .pipe(gulpif(argv.prod, htmlValidator()))
+    .pipe(dest('app'))
+};
 
 // Scripts & JS Libraries
 
@@ -82,20 +102,53 @@ function deploy() {
 	}))
 }
 
+// Делаем SVG спрайт
+
+function spriteSVG() {
+  return src('app/images/sprite/svg/*.svg')
+    .pipe(svgmin({
+      js2svg: {
+        pretty: true
+      }
+    }))
+    .pipe(cheerio({
+      run: function ($) {
+        $('[fill]').removeAttr('fill');
+        $('[stroke]').removeAttr('stroke');
+        $('[style]').removeAttr('style');
+      },
+      parserOptions: {xmlMode: true}
+    }))
+    .pipe(replace('&gt;', '>'))
+    .pipe(svgSprite({
+      mode: {
+        symbol: {
+          sprite: "sprite.svg"
+        }
+      }
+    }))
+    .pipe(dest('app/images/sprite'));
+};
+
+
 // Watching
 
 function startwatch() {
+	watch('app/pug/**/*', parallel('pug2html'));
 	watch('app/' + preprocessor + '/**/*', parallel('styles'));
 	watch(['app/**/*.js', '!app/js/*.min.js'], parallel('scripts'));
 	watch(['app/**/*.{' + imageswatch + '}'], parallel('images'));
+	watch(['app/images/sprite/svg/*'], parallel('spriteSVG'));
 	watch(['app/**/*.{' + fileswatch + '}']).on('change', browserSync.reload);
 }
 
 exports.browsersync = browsersync;
-exports.assets      = series(cleanimg, styles, scripts, images);
+exports.assets      = series(pug2html,cleanimg, styles, scripts, images, spriteSVG);
+exports.pug2html    = pug2html;
 exports.styles      = styles;
 exports.scripts     = scripts;
 exports.images      = images;
 exports.cleanimg    = cleanimg;
+exports.spriteSVG    = spriteSVG;
 exports.deploy      = deploy;
-exports.default     = parallel(images, styles, scripts, browsersync, startwatch);
+exports.default     = parallel(pug2html,images, styles, scripts, browsersync, spriteSVG, startwatch);
